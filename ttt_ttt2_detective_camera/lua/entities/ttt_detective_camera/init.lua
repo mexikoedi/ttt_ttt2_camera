0d271e4b14bbe9eb89adb1cd912b82720f1c5039
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-17 21:53:36 +0100

    formatted code

--- ttt_ttt2_detective_camera/lua/entities/ttt_detective_camera/init.lua
+++ ttt_ttt2_detective_camera/lua/entities/ttt_detective_camera/init.lua
@@ -1,47 +1,47 @@
-if engine.ActiveGamemode( ) != "terrortown" then return end
-AddCSLuaFile( "cl_init.lua" )
-AddCSLuaFile( "shared.lua" )
-include( "shared.lua" )
-util.AddNetworkString( "TTTCameraDetach" )
-util.AddNetworkString( "TTTCamera.Instructions" )
+if engine.ActiveGamemode() ~= "terrortown" then return end
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+util.AddNetworkString("TTTCameraDetach")
+util.AddNetworkString("TTTCamera.Instructions")
 
-function ENT:Use( user )
-    if user:IsDetective( ) && user == self:GetPlayer( ) then
-        self:Remove( )
+function ENT:Use(user)
+    if user:IsDetective() and user == self:GetPlayer() then
+        self:Remove()
 
-        if !self:GetShouldPitch( ) then
-            user:Give( "weapon_ttt_detective_camera" )
+        if not self:GetShouldPitch() then
+            user:Give("weapon_ttt_detective_camera")
         else
-            user:GetWeapon( "weapon_ttt_detective_camera" ).camera:SetShouldPitch( false )
-            user:GetWeapon( "weapon_ttt_detective_camera" ).camera:Remove( )
-            net.Start( "TTTCamera.Instructions" )
-            net.Send( user )
+            user:GetWeapon("weapon_ttt_detective_camera").camera:SetShouldPitch(false)
+            user:GetWeapon("weapon_ttt_detective_camera").camera:Remove()
+            net.Start("TTTCamera.Instructions")
+            net.Send(user)
         end
     end
 end
 
-function ENT:OnTakeDamage( dmginfo )
-    if self:GetShouldPitch( ) then return end
+function ENT:OnTakeDamage(dmginfo)
+    if self:GetShouldPitch() then return end
 
-    if dmginfo:GetDamageType( ) != DMG_BURN then
-        if IsValid( self:GetPlayer( ) ) && self:GetWelded( ) then
-            net.Start( "TTTCameraDetach" )
-            net.Send( self:GetPlayer( ) )
+    if dmginfo:GetDamageType() ~= DMG_BURN then
+        if IsValid(self:GetPlayer()) and self:GetWelded() then
+            net.Start("TTTCameraDetach")
+            net.Send(self:GetPlayer())
         end
 
-        constraint.RemoveAll( self )
-        self:SetWelded( false )
-        self:TakePhysicsDamage( dmginfo )
+        constraint.RemoveAll(self)
+        self:SetWelded(false)
+        self:TakePhysicsDamage(dmginfo)
     end
 
-    self.HP = self.HP - dmginfo:GetDamage( )
+    self.HP = self.HP - dmginfo:GetDamage()
 
     if self.HP <= 0 then
-        local ed = EffectData( )
-        ed:SetStart( self:GetPos( ) )
-        ed:SetOrigin( self:GetPos( ) )
-        ed:SetScale( 0.25 )
-        util.Effect( "HelicopterMegaBomb" , ed )
-        self:Remove( )
+        local ed = EffectData()
+        ed:SetStart(self:GetPos())
+        ed:SetOrigin(self:GetPos())
+        ed:SetScale(0.25)
+        util.Effect("HelicopterMegaBomb", ed)
+        self:Remove()
     end
 end
